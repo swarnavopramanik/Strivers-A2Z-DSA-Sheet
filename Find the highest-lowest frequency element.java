79dc7f5ff92fc23abaa51f35ef5baabe468dfa49
Swarnavo Pramanik <105142693+swarnavopramanik@users.noreply.github.com>
2023-06-28 09:45:15 +0530

    Create Find the highest-lowest frequency element.java

---
+++ Find the highest-lowest frequency element.java
@@ -0,0 +1,103 @@
+Example 1:
+Input: array[] = {10,5,10,15,10,5};
+Output: 10 15
+Explanation: The frequency of 10 is 3, i.e. the highest and the frequency of 15 is 1 i.e. the lowest.
+
+Example 2:
+
+Input: array[] = {2,2,3,4,4,2};
+Output: 2 3
+Explanation: The frequency of 2 is 3, i.e. the highest and the frequency of 3 is 1 i.e. the lowest.
+
+  Approach: 
+Brute-Force approach(Using two loops): 
+
+import java.util.*;
+
+public class tUf {
+
+    public static void main(String args[]) {
+
+        int arr[] = {10, 5, 10, 15, 10, 5};
+        int n = arr.length;
+        countFreq(arr, n);
+    }
+    public static void countFreq(int arr[], int n) {
+        boolean visited[] = new boolean[n];
+        int maxFreq = 0, minFreq = n;
+        int maxEle = 0, minEle = 0;
+        for (int i = 0; i < n; i++) {
+
+            // Skip this element if already processed
+            if (visited[i] == true)
+                continue;
+
+            // Count frequency
+            int count = 1;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] == arr[j]) {
+                    visited[j] = true;
+                    count++;
+                }
+            }
+            if (count > maxFreq) {
+                maxEle = arr[i];
+                maxFreq = count;
+            }
+            if (count < minFreq) {
+                minEle = arr[i];
+                minFreq = count;
+            }
+        }
+        System.out.println("The highest frequency element is: " + maxEle);
+        System.out.println("The lowest frequency element is: " + minEle);
+
+    }
+}  
+
+
+Optimized approach(Using map):
+
+  import java.util.*;
+
+public class Main {
+
+    public static void main(String args[]) {
+
+        int arr[] = {10, 5, 10, 15, 10, 5};
+        int n = arr.length;
+        Frequency(arr, n);
+    }
+    static void Frequency(int arr[], int n) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            if (map.containsKey(arr[i])) {
+                map.put(arr[i], map.get(arr[i]) + 1);
+            } else {
+                map.put(arr[i], 1);
+            }
+        }
+
+        int maxFreq = 0, minFreq = n;
+        int maxEle = 0, minEle = 0;
+        // Traverse through map and find the elements
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            int count = entry.getValue();
+            int element = entry.getKey();
+
+            if (count > maxFreq) {
+                maxEle = element;
+                maxFreq = count;
+            }
+            if (count < minFreq) {
+                minEle = element;
+                minFreq = count;
+            }
+        }
+
+        System.out.println("The highest frequency element is: " + maxEle);
+        System.out.println("The lowest frequency element is: " + minEle);
+    }
+} 
+
