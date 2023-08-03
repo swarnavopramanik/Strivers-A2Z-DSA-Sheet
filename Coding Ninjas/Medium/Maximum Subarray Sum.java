d255dbbc7f5e1a5b1573650e581c020e7ff0b92d
Swarnavo Pramanik <105142693+swarnavopramanik@users.noreply.github.com>
2023-08-03 13:45:02 +0530

    Update Maximum Subarray Sum.java

--- Coding Ninjas/Medium/Maximum Subarray Sum.java
+++ Coding Ninjas/Medium/Maximum Subarray Sum.java
@@ -1,1 +1,35 @@
+https://www.codingninjas.com/studio/problems/maximum-subarray-sum_630526?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTab=0
+
+import java.util.* ;
+import java.io.*; 
+
+public class Solution {
+	
+	public static long maxSubarraySum(int[] arr, int n) {
+		long maxi = Long.MIN_VALUE; // maximum sum
+        long sum = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            sum += arr[i];
+
+            if (sum > maxi) {
+                maxi = sum;
+            }
+
+            // If sum < 0: discard the sum calculated
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+
+        // To consider the sum of the empty subarray
+        // uncomment the following check:
+
+        //if (maxi < 0) maxi = 0;
+
+        return maxi;
+    }
+}
+
 
