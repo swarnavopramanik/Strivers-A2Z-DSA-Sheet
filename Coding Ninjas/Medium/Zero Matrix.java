272959762b88256c8ed3589b9159faed178b3e87
Swarnavo Pramanik <105142693+swarnavopramanik@users.noreply.github.com>
2023-08-03 13:48:47 +0530

    Create Zero Matrix.java

---
+++ Coding Ninjas/Medium/Zero Matrix.java
@@ -0,0 +1,36 @@
+https://www.codingninjas.com/studio/problems/zero-matrix_1171153?utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_Arrayproblems&leftPanelTab=0
+
+
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+    public static ArrayList<ArrayList<Integer>> zeroMatrix(ArrayList<ArrayList<Integer>> matrix, Integer n, Integer m) {
+    	int[] row = new int[n]; // row array
+        int[] col = new int[m]; // col array
+
+        // Traverse the matrix:
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix.get(i).get(j) == 0) {
+                    // mark ith index of row wih 1:
+                    row[i] = 1;
+
+                    // mark jth index of col wih 1:
+                    col[j] = 1;
+                }
+            }
+        }
+
+        // Finally, mark all (i, j) as 0
+        // if row[i] or col[j] is marked with 1.
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (row[i] == 1 || col[j] == 1) {
+                    matrix.get(i).set(j, 0);
+                }
+            }
+        }
+
+        return matrix;
+    }
+}
