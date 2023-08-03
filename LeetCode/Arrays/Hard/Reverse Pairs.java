d6133d3565e4e69c2dbd8770dda404326950e1d4
Swarnavo Pramanik <105142693+swarnavopramanik@users.noreply.github.com>
2023-08-03 22:38:10 +0530

    Create Reverse Pairs.java

---
+++ LeetCode/Arrays/Hard/Reverse Pairs.java
@@ -0,0 +1,58 @@
+493. Reverse Pairs
+  // Hard
+
+
+  public class Solution {
+
+    public int reversePairs(int[] nums) {
+        return mergeSort(nums, 0, nums.length - 1);
+    }
+
+    private int mergeSort(int[] arr, int low, int high) {
+        int cnt = 0;
+        if (low >= high) return cnt;
+        int mid = (low + high) / 2;
+        cnt += mergeSort(arr, low, mid);
+        cnt += mergeSort(arr, mid + 1, high);
+        cnt += countPairs(arr, low, mid, high);
+        merge(arr, low, mid, high);
+        return cnt;
+    }
+
+    private void merge(int[] arr, int low, int mid, int high) {
+        int[] temp = new int[high - low + 1];
+        int left = low;
+        int right = mid + 1;
+        int index = 0;
+
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right]) {
+                temp[index++] = arr[left++];
+            } else {
+                temp[index++] = arr[right++];
+            }
+        }
+
+        while (left <= mid) {
+            temp[index++] = arr[left++];
+        }
+
+        while (right <= high) {
+            temp[index++] = arr[right++];
+        }
+
+        System.arraycopy(temp, 0, arr, low, temp.length);
+    }
+
+    private int countPairs(int[] arr, int low, int mid, int high) {
+        int right = mid + 1;
+        int cnt = 0;
+        for (int i = low; i <= mid; i++) {
+            while (right <= high && (long) arr[i] > 2L * arr[right]) {
+                right++;
+            }
+            cnt += (right - (mid + 1));
+        }
+        return cnt;
+    }
+}
