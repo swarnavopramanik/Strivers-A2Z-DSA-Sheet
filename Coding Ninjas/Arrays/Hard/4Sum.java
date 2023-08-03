d48f3d187f730d318be550948e893ddd702357de
Swarnavo Pramanik <105142693+swarnavopramanik@users.noreply.github.com>
2023-08-03 22:28:30 +0530

    Update 4Sum.java

--- Coding Ninjas/Arrays/Hard/4Sum.java
+++ Coding Ninjas/Arrays/Hard/4Sum.java
@@ -1,1 +1,37 @@
+https://www.codingninjas.com/studio/problems/4sum_5713771?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTab=0
 
+
+import java.util.*;
+public class Solution {
+   public static List<List<Integer>> fourSum(int[] nums, int target) {
+        int n = nums.length; // size of the array
+        Set<List<Integer>> st = new HashSet<>();
+
+        // checking all possible quadruplets:
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                Set<Long> hashset = new HashSet<>();
+                for (int k = j + 1; k < n; k++) {
+                    // taking bigger data type
+                    // to avoid integer overflow:
+                    long sum = nums[i] + nums[j];
+                    sum += nums[k];
+                    long fourth = target - sum;
+                    if (hashset.contains(fourth)) {
+                        List<Integer> temp = new ArrayList<>();
+                        temp.add(nums[i]);
+                        temp.add(nums[j]);
+                        temp.add(nums[k]);
+                        temp.add((int) fourth);
+                        temp.sort(Integer::compareTo);
+                        st.add(temp);
+                    }
+                    // put the kth element into the hashset:
+                    hashset.add((long) nums[k]);
+                }
+            }
+        }
+        List<List<Integer>> ans = new ArrayList<>(st);
+        return ans;
+    }
+}
