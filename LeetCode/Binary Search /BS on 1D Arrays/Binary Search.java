0036dc82d8eb1a83d72320174a0639400e08cb4a
Swarnavo Pramanik <105142693+swarnavopramanik@users.noreply.github.com>
2023-08-03 15:06:33 +0530

    Create Binary Search.java

---
+++ LeetCode/Binary Search /BS on 1D Arrays/Binary Search.java
@@ -0,0 +1,18 @@
+704. Binary Search
+
+
+  class Solution {
+    public int search(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] >= target)
+                r = mid;
+            else
+                l = mid + 1;  
+        }
+
+        if (nums[l] != target) return -1;
+        return l;
+    }
+}
