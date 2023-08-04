d5a14f072b3791bbb0f2a77aded8a3329e7bc2a4
Swarnavo Pramanik <105142693+swarnavopramanik@users.noreply.github.com>
2023-08-04 22:40:42 +0530

    Update Square Root of a number.java

--- Coding Ninjas/Binary Search/ BS on Answers/Square Root of a number.java
+++ Coding Ninjas/Binary Search/ BS on Answers/Square Root of a number.java
@@ -1,1 +1,13 @@
+https://www.codingninjas.com/studio/problems/square-root-integral_893351?leftPanelTab=1&utm_medium=website&utm_campaign=a_zcoursetuf
 
+
+import java.util.* ;
+import java.io.*; 
+
+public class Solution {
+
+	public static int sqrtN(long N) {
+		int ans = (int) Math.sqrt(N);
+        return ans;
+	}
+}
