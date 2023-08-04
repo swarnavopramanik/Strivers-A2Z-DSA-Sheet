6ae1e7cc9d9f58cf8cf7ca06d6bbdaf092fb4463
Swarnavo Pramanik <105142693+swarnavopramanik@users.noreply.github.com>
2023-08-04 22:18:59 +0530

    Create Implement Upper Bound.java

---
+++ Coding Ninjas/Binary Search/BS on 1D Arrays/Implement Upper Bound.java
@@ -0,0 +1,22 @@
+https://www.codingninjas.com/studio/problems/implement-upper-bound_8165383?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTab=0
+
+
+public class Solution {
+     public static int upperBound(int[] arr, int x, int n) {
+        int low = 0, high = n - 1;
+        int ans = n;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            // maybe an answer
+            if (arr[mid] > x) {
+                ans = mid;
+                //look for smaller index on the left
+                high = mid - 1;
+            } else {
+                low = mid + 1; // look on the right
+            }
+        }
+        return ans;
+    }
+}
