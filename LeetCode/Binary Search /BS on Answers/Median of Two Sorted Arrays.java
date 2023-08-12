50da1209b437ede86ae1c04dfbd87bf96b6efcad
Swarnavo Pramanik <105142693+swarnavopramanik@users.noreply.github.com>
2023-08-12 13:56:58 +0530

    Create Median of Two Sorted Arrays.java

---
+++ LeetCode/Binary Search /BS on Answers/Median of Two Sorted Arrays.java
@@ -0,0 +1,90 @@
+4. Median of Two Sorted Arrays
+Hard
+
+    // Time Complexity: O(log(min(n1,n2)))
+  // Beats 100.00%
+
+  class Solution {
+  public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n1 = nums1.length, n2 = nums2.length;
+        //if n1 is bigger swap the arrays:
+        if (n1 > n2) return findMedianSortedArrays(nums2, nums1);
+
+        int n = n1 + n2; //total length
+        int left = (n1 + n2 + 1) / 2; //length of left half
+        
+        //apply binary search:
+        int low = 0, high = n1;
+        while (low <= high) {
+            int mid1 = (low + high) / 2;
+            int mid2 = left - mid1;
+            //calculate l1, l2, r1 and r2;
+            int l1 = (mid1 > 0) ? nums1[mid1 - 1] : Integer.MIN_VALUE;
+            int l2 = (mid2 > 0) ? nums2[mid2 - 1] : Integer.MIN_VALUE;
+            int r1 = (mid1 < n1) ? nums1[mid1] : Integer.MAX_VALUE;
+            int r2 = (mid2 < n2) ? nums2[mid2] : Integer.MAX_VALUE;
+
+            if (l1 <= r2 && l2 <= r1) {
+                if (n % 2 == 1) return Math.max(l1, l2);
+                else return ((double) (Math.max(l1, l2) + Math.min(r1, r2))) / 2.0;
+            } else if (l1 > r2) high = mid1 - 1;
+            else low = mid1 + 1;
+        }
+        return 0; //dummy statement
+    }
+  }
+
+
+// Beats 91.23%
+
+/* class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        int n = n1 + n2;
+        int[] new_arr = new int[n];
+
+        int i=0, j=0, k=0;
+
+        while (i<=n1 && j<=n2) {
+            if (i == n1) {
+                while(j<n2) new_arr[k++] = nums2[j++];
+                break;
+            } else if (j == n2) {
+                while (i<n1) new_arr[k++] = nums1[i++];
+                break;
+            }
+
+            if (nums1[i] < nums2[j]) {
+                new_arr[k++] = nums1[i++];
+            } else {
+                new_arr[k++] = nums2[j++];
+            }
+        }
+
+        if (n%2==0) return (float)(new_arr[n/2-1] + new_arr[n/2])/2;
+        else return new_arr[n/2];
+    }
+} 
+ */
+
+// Beats 36.57% 
+
+ /* class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int[] arr = new int[nums1.length+nums2.length];
+        int k =0;
+        for(int i =0;i<nums1.length;i++) {
+            arr[k++] =nums1[i];
+        }
+        for(int i =0;i<nums2.length;i++) {
+            arr[k++] =nums2[i];
+        }
+        Arrays.sort(arr);
+        int mid = arr.length/2;
+        if(arr.length%2==0) {
+           return (double)(arr[mid-1] + arr[mid])/2;
+        } 
+        return arr[mid];
+    }
+}  */ 
