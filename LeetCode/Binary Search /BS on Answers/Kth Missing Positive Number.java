78f40bb80a467f2b67225772b6c24a20edfc1edf
Swarnavo Pramanik <105142693+swarnavopramanik@users.noreply.github.com>
2023-08-12 13:55:40 +0530

    Create Kth Missing Positive Number.java

---
+++ LeetCode/Binary Search /BS on Answers/Kth Missing Positive Number.java
@@ -0,0 +1,17 @@
+1539. Kth Missing Positive Number
+
+  class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        int low = 0, high = arr.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int missing = arr[mid] - (mid + 1);
+            if (missing < k) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return k + high + 1;
+    }
+}
