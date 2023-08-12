864c4c13a3aabf87158e9f13a3dfd29ebff6fc73
Swarnavo Pramanik <105142693+swarnavopramanik@users.noreply.github.com>
2023-08-12 13:54:34 +0530

    Create  Find the Smallest Divisor.java

---
+++ LeetCode/Binary Search /BS on Answers/Find the Smallest Divisor.java
@@ -0,0 +1,40 @@
+1283. Find the Smallest Divisor Given a Threshold
+
+  // Salesforce
+
+class Solution {
+
+    public static int sumByD(int[] nums, int div) {
+        int n = nums.length; //size of array
+        //Find the summation of division values:
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += Math.ceil((double)(nums[i]) / (double)(div));
+        }
+        return sum;
+    }
+
+    public int smallestDivisor(int[] nums, int threshold) {
+        
+         int n = nums.length; //size of array.
+        if(n > threshold) return -1;
+        //Find the maximum element:
+        int maxi = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            maxi = Math.max(maxi, nums[i]);
+        }
+
+        int low = 1, high = maxi;
+
+        //Apply binary search:
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (sumByD(nums, mid) <= threshold) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+}
