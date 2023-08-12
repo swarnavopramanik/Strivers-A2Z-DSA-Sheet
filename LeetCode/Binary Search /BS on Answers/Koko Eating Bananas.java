d9e2fca9d98782f298b0ae1b2cd54944d0bcfbac
Swarnavo Pramanik <105142693+swarnavopramanik@users.noreply.github.com>
2023-08-12 13:52:57 +0530

    Update Koko Eating Bananas.java

--- LeetCode/Binary Search /BS on Answers/Koko Eating Bananas.java
+++ LeetCode/Binary Search /BS on Answers/Koko Eating Bananas.java
@@ -1,1 +1,43 @@
+875. Koko Eating Bananas
 
+
+  class Solution {
+
+    public static int findMax(int[] piles) {
+        int maxi = Integer.MIN_VALUE;;
+        int n = piles.length;
+        //find the maximum:
+        for (int i = 0; i < n; i++) {
+            maxi = Math.max(maxi, piles[i]);
+        }
+        return maxi;
+    }
+
+
+     public static int calculateTotalHours(int[] piles, int hourly) {
+        int totalH = 0;
+        int n = piles.length;
+        //find total hours:
+        for (int i = 0; i < n; i++) {
+            totalH += Math.ceil((double)(piles[i]) / (double)(hourly));
+        }
+        return totalH;
+    }
+
+
+    public int minEatingSpeed(int[] piles, int h) {
+         int low = 1, high = findMax(piles);
+
+         //apply binary search:
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int totalH = calculateTotalHours(piles, mid);
+            if (totalH <= h) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+}
