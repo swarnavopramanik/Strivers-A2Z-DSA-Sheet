a07fe46ce87ce2d54dedc27ecf5d5977b2bb2ea1
Swarnavo Pramanik <105142693+swarnavopramanik@users.noreply.github.com>
2023-08-12 13:58:19 +0530

    Update Aggressive Cows.java

--- Coding Ninjas/Binary Search/ BS on Answers/Aggressive Cows.java
+++ Coding Ninjas/Binary Search/ BS on Answers/Aggressive Cows.java
@@ -1,29 +1,34 @@
 https://www.codingninjas.com/studio/problems/aggressive-cows_1082559?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTab=0
 
 
-bool canWePlace(vector<int> &stalls, int dist, int cows) {
-    int n = stalls.size(); //size of array
-    int cntCows = 1; //no. of cows placed
-    int last = stalls[0]; //position of last placed cow.
-    for (int i = 1; i < n; i++) {
-        if (stalls[i] - last >= dist) {
-            cntCows++; //place next cow.
-            last = stalls[i]; //update the last location.
+import java.util.*;
+public class Solution {
+    public static boolean canWePlace(int[] stalls, int dist, int cows) {
+        int n = stalls.length; //size of array
+        int cntCows = 1; //no. of cows placed
+        int last = stalls[0]; //position of last placed cow.
+        for (int i = 1; i < n; i++) {
+            if (stalls[i] - last >= dist) {
+                cntCows++; //place next cow.
+                last = stalls[i]; //update the last location.
+            }
+            if (cntCows >= cows) return true;
         }
-        if (cntCows >= cows) return true;
+        return false;
     }
-    return false;
-}
-int aggressiveCows(vector<int> &stalls, int k) {
-    int n = stalls.size(); //size of array
-    //sort the stalls[]:
-    sort(stalls.begin(), stalls.end());
+    public static int aggressiveCows(int[] stalls, int k) {
+        int n = stalls.length; //size of array
+        //sort the stalls[]:
+        Arrays.sort(stalls);
 
-    int limit = stalls[n - 1] - stalls[0];
-    for (int i = 1; i <= limit; i++) {
-        if (canWePlace(stalls, i, k) == false) {
-            return (i - 1);
+        int low = 1, high = stalls[n - 1] - stalls[0];
+        //apply binary search:
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (canWePlace(stalls, mid, k) == true) {
+                low = mid + 1;
+            } else high = mid - 1;
         }
+        return high;
     }
-    return limit;
 }
