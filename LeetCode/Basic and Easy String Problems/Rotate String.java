1e449bcabc8a0bb037faeac8295888d145a9b2f1
Swarnavo Pramanik <105142693+swarnavopramanik@users.noreply.github.com>
2023-08-14 12:45:08 +0530

    Create Rotate String.java

---
+++ LeetCode/Basic and Easy String Problems/Rotate String.java
@@ -0,0 +1,44 @@
+796. Rotate String
+
+   class Solution1 {
+    public boolean rotateString(String s, String goal) {
+        if(s ==null || goal == null){
+            return false;
+        }
+        if(s.length() != goal.length()) return false;
+        if(s.length() == 0){
+            return true;
+        }
+        int i =0, j =0;
+        while(i < s.length() && j < goal.length()){
+            if(s.charAt(i) == goal.charAt(j)){
+                i++; j++;
+            }
+            else{
+                if(j == 0){
+                    i++;
+                }
+                else{
+                    j= 0;
+                }
+            }
+        }
+        return (s.substring(0,goal.length() - j).equals(goal.substring(j)));
+    }
+} 
+
+
+ class Solution2 {
+    public boolean rotateString(String s, String goal) {
+        if(s ==null || goal == null){
+            return false;
+        }
+        if(s.length() != goal.length()) return false;
+        String p=s+s;
+        int x = p.indexOf(goal);
+        if(x==-1){
+            return false;
+        }
+        return true;
+    }
+} 
