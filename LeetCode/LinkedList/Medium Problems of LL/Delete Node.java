f3af568b67012be7147c89f67bdd04a303f0aa05
Swarnavo Pramanik <105142693+swarnavopramanik@users.noreply.github.com>
2023-08-17 19:39:36 +0530

    Create Delete Node.java

---
+++ LeetCode/LinkedList/Medium Problems of LL/Delete Node.java
@@ -0,0 +1,23 @@
+237. Delete Node in a Linked List
+
+
+  class Solution {
+    public void deleteNode(ListNode node) {
+     node.val=node.next.val;
+     node.next=node.next.next;
+      
+    }
+}
+
+  class Solution {
+    public void deleteNode(ListNode node) {
+ListNode temp = new ListNode();
+        while(node.next !=null)        {
+            node.val = node.next.val;
+             temp = node;
+            node= node.next;
+        }
+
+        temp.next = null;
+    }
+} 
