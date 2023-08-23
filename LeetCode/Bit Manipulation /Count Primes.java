ce1dc62ed5a664c7518959ab4caa32e9cf890fd0
Swarnavo Pramanik <105142693+swarnavopramanik@users.noreply.github.com>
2023-08-23 18:29:41 +0530

    Create Count Primes.java

---
+++ LeetCode/Bit Manipulation /Count Primes.java
@@ -0,0 +1,24 @@
+204. Count Primes
+
+
+  class Solution {
+    public int countPrimes(int n) {
+        
+     boolean[] isprime = new boolean[n];
+     int count = 0;
+        
+        for(int i = 2; i*i<=n; i++){
+            if(!isprime[i]){
+                    for(int j = i*2; j<n; j+=i){
+                        isprime[j] = true;
+                    }
+            }
+        }
+        for(int i=2;i<isprime.length;i++){
+            if(!isprime[i]){
+                count++;
+            }
+        }
+        return count;
+    }
+}
