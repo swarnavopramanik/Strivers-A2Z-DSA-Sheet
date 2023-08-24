d1831a5fa5740e74447a7b989c182881fe131410
Swarnavo Pramanik <105142693+swarnavopramanik@users.noreply.github.com>
2023-08-24 20:45:14 +0530

    Create  Largest Rectangle.java

---
+++ LeetCode/ Stack and Queues/ Monotonic Stack & Queue Problems [VVV. Imp]/Largest Rectangle.java
@@ -0,0 +1,23 @@
+84. Largest Rectangle in Histogram
+
+  class Solution {
+    public int largestRectangleArea(int[] heights) {
+        Stack < Integer > st = new Stack < > ();
+        int maxA = 0;
+        int n = heights.length;
+        for (int i = 0; i <= n; i++) {
+        while (!st.empty() && (i == n || heights[st.peek()] >= heights[i])) {
+                int height = heights[st.peek()];
+                st.pop();
+                int width;
+                if (st.empty())
+                    width = i;
+                else
+                    width = i - st.peek() - 1;
+                maxA = Math.max(maxA, width * height);
+            }
+            st.push(i);
+        }
+        return maxA;
+    }
+}
