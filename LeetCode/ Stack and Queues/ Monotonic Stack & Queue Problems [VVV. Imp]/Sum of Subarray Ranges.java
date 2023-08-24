c4639bd1ec48413d4dfc132475185ac0bdc60180
Swarnavo Pramanik <105142693+swarnavopramanik@users.noreply.github.com>
2023-08-24 20:44:00 +0530

    Create Sum of Subarray Ranges.java

---
+++ LeetCode/ Stack and Queues/ Monotonic Stack & Queue Problems [VVV. Imp]/Sum of Subarray Ranges.java
@@ -0,0 +1,16 @@
+2104. Sum of Subarray Ranges
+
+  class Solution {
+        public long subArrayRanges(int[] A) {
+        long res = 0;
+        for (int i = 0; i < A.length; i++) {
+            int max = A[i], min = A[i];
+            for (int j = i; j < A.length; j++) {
+                max = Math.max(max, A[j]);
+                min = Math.min(min, A[j]);
+                res += max - min;
+            }
+        }
+        return res;
+    }
+}
