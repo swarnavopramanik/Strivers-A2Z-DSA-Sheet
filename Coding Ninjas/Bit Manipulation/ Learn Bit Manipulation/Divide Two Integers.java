5353e7d09f87030646935a7066eca96a709c6d4a
Swarnavo Pramanik <105142693+swarnavopramanik@users.noreply.github.com>
2023-09-28 22:35:28 +0530

    Create Divide Two Integers.java

---
+++ Coding Ninjas/Bit Manipulation/ Learn Bit Manipulation/Divide Two Integers.java
@@ -0,0 +1,10 @@
+https://www.codingninjas.com/studio/problems/-divide-two-integers_1112617?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf
+
+public class Solution{
+    public static int divideTwoInteger(int dividend, int divisor) {
+        // Write your code here.
+         int ans = dividend/divisor;
+
+    return ans;
+    }
+}
