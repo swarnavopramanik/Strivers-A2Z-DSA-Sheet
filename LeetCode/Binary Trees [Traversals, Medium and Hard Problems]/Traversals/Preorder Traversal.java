d28932b589049e2e654e2377584d301b6bbd5c8d
Swarnavo Pramanik <105142693+swarnavopramanik@users.noreply.github.com>
2023-12-25 11:05:40 +0530

    Create Preorder Traversal.java

---
+++ LeetCode/Binary Trees [Traversals, Medium and Hard Problems]/Traversals/Preorder Traversal.java
@@ -0,0 +1,18 @@
+144. Binary Tree Preorder Traversal
+
+  class Solution {
+    List<Integer> preorderTraverse(TreeNode root,List<Integer> list) {
+
+        if(root==null)
+            return list;
+        list.add(root.val);
+        preorderTraverse(root.left,list);
+        preorderTraverse(root.right,list);
+        return list;
+    }
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<Integer>();
+        list = preorderTraverse(root,list);
+        return list;
+    }
+} 
