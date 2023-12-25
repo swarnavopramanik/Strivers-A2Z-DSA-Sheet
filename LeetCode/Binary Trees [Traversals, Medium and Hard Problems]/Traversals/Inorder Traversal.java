a4ad0daf077e0b12154465a67295d19db1f55c55
Swarnavo Pramanik <105142693+swarnavopramanik@users.noreply.github.com>
2023-12-25 11:03:08 +0530

    Create Inorder Traversal.java

---
+++ LeetCode/Binary Trees [Traversals, Medium and Hard Problems]/Traversals/Inorder Traversal.java
@@ -0,0 +1,14 @@
+94. Binary Tree Inorder Traversal
+
+  class Solution {
+    private List<Integer> ans = new ArrayList<>();
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if(root==null) {
+            return ans;
+        }
+        inorderTraversal(root.left);
+        ans.add(root.val);
+        inorderTraversal(root.right);
+        return ans;
+    }
+}
