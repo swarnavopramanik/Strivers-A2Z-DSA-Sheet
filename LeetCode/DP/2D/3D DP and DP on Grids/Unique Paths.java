230599287ef5afcc19dfe9564f94b4c57ecb46e6
Swarnavo Pramanik <105142693+swarnavopramanik@users.noreply.github.com>
2024-04-08 11:47:01 +0530

    Update Unique Paths.java

--- LeetCode/DP/2D/3D DP and DP on Grids/Unique Paths.java
+++ LeetCode/DP/2D/3D DP and DP on Grids/Unique Paths.java
@@ -1,5 +1,32 @@
 62. Unique Paths
+
+
+  // Striver 
+
+class Solution {
+    public int uniquePathsUtill(int i,int j,int dp[][]){
+        if(i == 0 && j == 0)
+        return 1;
+        if(i < 0 || j < 0)
+        return 0;
+        if(dp[i][j] != -1)
+        return dp[i][j];
+
+        int up = uniquePathsUtill(i-1,j, dp);
+        int left = uniquePathsUtill(i, j-1, dp);
+
+        return dp[i][j] = up + left;
+    }
+    
+
   
+    public int uniquePaths(int m, int n){
+        int dp[][] = new int[m][n];
+        for(int[] row : dp)
+        Arrays.fill(row, -1);
+        return uniquePathsUtill(m-1, n-1, dp);
+    }
+}
 class Solution {
     public int uniquePaths(int m, int n){
         int[][] dp = new int[m][n];
